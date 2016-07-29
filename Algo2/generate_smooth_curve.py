64d4d44a2dc90992992ee33e96d1f8727481530c
JeanBilheux <bilheuxjm@ornl.gov>
2016-07-29 09:02:17 -0400

    Started to work on algo2

---
+++ Algo2/generate_smooth_curve.py
@@ -0,0 +1,19 @@
+import numpy as np
+# generate smoothed curve based on window
+
+
+def generate_smooth_curve(x, y):
+    x2 = x
+    y2 = np.zeros(x2.size)
+    tolerance = np.mean(np.diff(x2)) / 100
+    for i in range(0, y2.size):
+        index = np.nonzero(x == x2[i])[0][0]
+        min = index
+        max = index
+        while min > 0 and x[index] - x[min - 1] <= window + tolerance:
+            min -= 1
+        while max < y.size - 1 and x[max + 1] - x[index] <= window + tolerance:
+            max += 1
+        y2[i] = np.mean(y[min:max+1])
+        
+    return {'x2': x2, 'y2': y2}
