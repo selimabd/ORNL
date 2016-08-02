fc172d4820464d38b9bad444014f4bc5f1e39859
JeanBilheux <bilheuxjm@ornl.gov>
2016-08-02 11:35:27 -0400

    Added last algo

--- utilities/diff_algorithm.py
+++ utilities/diff_algorithm.py
@@ -13,12 +13,40 @@ def calculate_diff_between_input_and_curve(x, y, x2, poly):
     return ydiff
 
 
-def calculate_jumps(t, ydiff, x2, x):
+def calculate_jumps(t, ydiff, x2, x, y=None, method=1):
     '''
     identify max points of regions exceeding threshold
     '''
-    
+
     jumps = np.extract(ydiff > t, x2)
+    if method == 2:
+        return calculate_jumps_method_1(t, y, x2, x, jumps)
+    else:    
+        return calculate_jumps_method_1(t, ydiff, x2, x, jumps)
+
+    
+def calculate_jumps_method_1(t, ydiff, x2, x, jumps):
+        
+    maxjumps = np.array([])
+    i = 0
+    while i < jumps.size:
+        area = 1
+        max = ydiff[np.nonzero(x2 == jumps[i])[0][0]]
+        maxi = i
+        while ((i + area) < (jumps.size - 1)) and (jumps[i + area] == x[np.nonzero(x == jumps[i])[0][0] + area]):
+            if (ydiff[np.nonzero(x2 == jumps[i + area])[0][0]]) > max:
+                max = ydiff[np.nonzero(x2 == jumps[i + area])[0][0]]
+                maxi = i + area
+            area += 1
+        maxjumps = np.append(maxjumps, jumps[maxi])
+        i += area + 1
+    print('Maximal points exceeding threshold {}: {}'.format(round(t, 5), maxjumps))
+#    with open(thresholdOutput, "a") as file:
+#        file.write('Maximal points exceeding threshold {}: {}\n'.format(round(t, 5), maxjumps))
+    return maxjumps
+
+def calculate_jumps_method_2(t, ydiff, x2, jumps):
+        
     maxjumps = np.array([])
     i = 0
     while i < jumps.size:
